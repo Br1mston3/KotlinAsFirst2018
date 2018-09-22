15282d10b5931d658462df1a182e382d601b0dc9
Br1mston3 <daniil_smirnov_2000@mail.ru>
2018-09-22 20:02:00 +0300

    выполнен 1 урок

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -93,12 +93,8 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = (sqrt(
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int {
-    var number = number / 100
-    if (number > 10)
-        number = (number - 10)
-    return number
-}
+fun thirdDigit(number: Int): Int = number % 1000/100
+
 
 /**
  * Простая
@@ -116,7 +112,7 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
+fun accountInThreeYears(initial: Int, percent: Int): Double = (initial * (1 + percent / 100.0) * (1 + percent / 100.0) * (1 + percent / 100.0))
 
 /**
  * Простая
@@ -124,4 +120,4 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = TODO()
+fun numberRevert(number: Int): Int = 100*number % 10 + (number / 10) % 10 + number/100
